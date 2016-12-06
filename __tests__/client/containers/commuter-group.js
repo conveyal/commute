7b076661780e192885930013c8f09904b5b46275
Evan Siroky <evan.siroky@yahoo.com>
2016-12-05 16:08:45 -0800

    test(client-app): add test in commuter-group view
    - add test for deleting commuter from commuter-group view

--- __tests__/client/containers/commuter-group.js
+++ __tests__/client/containers/commuter-group.js
@@ -6,10 +6,16 @@ import pretty from 'pretty'
 import React from 'react'
 import {Provider} from 'react-redux'
 
-import {makeMockStore, mockStores} from '../../test-utils/mock-data.js'
+import {makeGenericModelActionsExpectations} from '../../test-utils/actions'
+import {makeMockStore, mockCommuter, mockStores} from '../../test-utils/mock-data.js'
 import '../../test-utils/mock-leaflet'
 
 import CommuterGroup from '../../../client/containers/commuter-group'
+
+const commuterExpectations = makeGenericModelActionsExpectations({
+  pluralName: 'commuters',
+  singularName: 'commuter'
+})
 
 describe('Container > CommuterGroup', () => {
   it('Commuter Group View loads', () => {
@@ -36,13 +42,40 @@ describe('Container > CommuterGroup', () => {
 
   /* it('Edit commuter group name', () => {
     throw new Error('unimplemented')
-  })
+  }) */
 
   it('Delete Commuter', () => {
-    throw new Error('unimplemented')
+    const mockStore = makeMockStore(mockStores.withAnalysisRun)
+    window.confirm = () => true
+
+    // Given a logged-in user is viewing the commuter group view
+    // mount component
+    const tree = mount(
+      <Provider store={mockStore}>
+        <CommuterGroup
+          params={{groupId: 'group-2'}}
+          />
+      </Provider>
+      , {
+        attachTo: document.getElementById('test')
+      }
+    )
+
+    // clear all loading actions
+    mockStore.clearActions()
+
+    // When the user clicks the delete button for an existing site
+    // And the user confirms the Confirm Deletion dialog
+    const deleteButton = tree.find('table').at(1).find('button').last()
+    deleteButton.simulate('click')
+
+    commuterExpectations.expectDeleteAction({
+      action: mockStore.getActions()[0],
+      entity: mockCommuter
+    })
   })
 
-  it('Show Commuter in Ridematch', () => {
+  /* it('Show Commuter in Ridematch', () => {
     throw new Error('unimplemented')
   }) */
 })
