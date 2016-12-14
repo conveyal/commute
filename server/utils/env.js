c987b33e81feda8710f39347b32ef82d0c71e8ea
Evan Siroky <evan.siroky@yahoo.com>
2016-12-13 23:00:03 -0800

    chore(heroku): use environment variables instead of env file with Heroku

--- server/utils/env.js
+++ server/utils/env.js
@@ -5,8 +5,19 @@ const YAML = require('yamljs')
 const envFolder = process.env.NODE_ENV === 'test' ? 'test' : 'default'
 const configurationsFolder = path.resolve(`${__dirname}/../../configurations/`)
 
+// create env object with environment vars if in Heroku environment
+let envObj
+if (process.env.MONGODB_URI) {
+  envObj = {
+    MAPZEN_SEARCH_KEY: process.env.MAPZEN_SEARCH_KEY,
+    OTP_URL: process.env.OTP_URL
+  }
+} else {
+  envObj = YAML.load(`${configurationsFolder}/${envFolder}/env.yml`)
+}
+
 const env = {
-  env: YAML.load(`${configurationsFolder}/${envFolder}/env.yml`),
+  env: envObj,
   messages: YAML.load(`${configurationsFolder}/default/messages.yml`),
   settings: YAML.load(`${configurationsFolder}/default/settings.yml`)
 }
