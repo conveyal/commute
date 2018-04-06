b93c1f5aee1e4deaf6ad38624e17535ec0f9ac7e
evansiroky <evan.siroky@yahoo.com>
2018-04-06 13:34:51 -0700

    refactor(auth0): fix listing error

--- client/utils/auth0.js
+++ client/utils/auth0.js
@@ -1,6 +1,6 @@
 import Auth0Lock from 'auth0-lock'
 
-export function getLock (options={}) {
+export function getLock (options = {}) {
   return new Auth0Lock(
     process.env.AUTH0_CLIENT_ID,
     process.env.AUTH0_DOMAIN,
